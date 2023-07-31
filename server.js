61884c0c3f6727efa9a0889f1ff39bb1697302bd
fzhao888 <frank.zhao93@gmail.com>
2023-07-31 02:54:23 -0500

    Added node.js code for viewDepartments

--- server.js
+++ server.js
@@ -1,49 +1,56 @@
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
+const cTable = require('console.table');
 
 // Uses inquirer to perform the initial prompt
-inquirer.
-    prompt([
-        {
-            type: 'list',
-            name: 'option',
-            message: 'Please choose an option: ',
-            choices: [
-                'View all department',
-                'View all roles',
-                'View all employees',
-                'Add a department',
-                'Add a role',
-                'Add an employee',
-                'Update an employee role'
-            ]
-        }
-    ])
-    .then((data) => {
-        switch (data.option) {
-            case 'View all department':
-                viewDepartments();
-                break;
-            case 'View all roles':
-                viewRoles()
-                break;
-            case 'View all employees':
-                viewEmployees();
-                break;
-            case 'Add a department':
-                addADepartment();
-                break;
-            case 'Add a role':
-                addRole();
-                break;
-            case 'Add an employee':
-                addEmployee();
-                break;
-            case 'Update an employee role':
-                updateEmployeeRole();
-                break;
-        }
-    });
+function prompt() {
+    inquirer.
+        prompt([
+            {
+                type: 'list',
+                name: 'option',
+                message: 'Please choose an option: ',
+                choices: [
+                    'View all department',
+                    'View all roles',
+                    'View all employees',
+                    'Add a department',
+                    'Add a role',
+                    'Add an employee',
+                    'Update an employee role',
+                    'Exit'
+                ]
+            }
+        ])
+        .then((data) => {
+            switch (data.option) {
+                case 'View all department':
+                    viewDepartments();
+                    break;
+                case 'View all roles':
+                    viewRoles()
+                    break;
+                case 'View all employees':
+                    viewEmployees();
+                    break;
+                case 'Add a department':
+                    addADepartment();
+                    break;
+                case 'Add a role':
+                    addRole();
+                    break;
+                case 'Add an employee':
+                    addEmployee();
+                    break;
+                case 'Update an employee role':
+                    updateEmployeeRole();
+                    break;
+                case 'Exit':
+                    db.end();
+                    break;
+            }
+        });
+}
 
 // Connect to database
 const db = mysql.createConnection(
@@ -51,22 +58,23 @@ const db = mysql.createConnection(
         host: 'localhost',
         user: 'root',
         password: 'vegetable',
-        database: 'employee_db'
-    },
-    console.log(`Welcome to the Employee Manager!`)
-).promise();
+        database: 'company_db'
+    }
+);
+prompt();
 
-function viewDepartments() {
-    const sql = `SELECT * FROM department;`
-    db.query(sql, (err, results) => {
-        if (err) {
-            console.error(err);
-            return;
-        }
-        console.log('Displaying departments: ');
-        console.log(results);
+async function viewDepartments() {
+    console.log('\nDisplaying departments: \n ');
+    const sql = `SELECT *
+     FROM departments;`
 
-    });
+    try {
+        const [results] = await db.promise().query(sql);
+        console.table(results);
+    } catch (err) {
+        console.log(err);
+    }
+    prompt();
 }
 
 function viewRoles() {
